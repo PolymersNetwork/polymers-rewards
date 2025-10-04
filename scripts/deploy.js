5a80122650c101ceda15304741e28bca379c3629
Polymers <polymers.network@gmail.com>
2025-10-04 10:38:42 +0300

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,10 @@
+const { ethers } = require("hardhat");
 
+async function main() {
+  const WrappedEsgNFT = await ethers.getContractFactory("WrappedEsgNFT");
+  const contract = await WrappedEsgNFT.deploy(process.env.ETHEREUM_WORMHOLE);
+  await contract.deployed();
+  console.log("WrappedEsgNFT deployed to:", contract.address);
+}
+
+main();
